d03f132b8cfbe43a4281317a52e89fa3b6ebc823
ChamilYahampath <chamil1012bandara@gmail.com>
2024-05-14 14:52:39 +0530

    order API done

---
+++ src/main/java/com/chamil/ShopMate/controller/AdminOrderController.java
@@ -0,0 +1,47 @@
+package com.chamil.ShopMate.controller;
+
+import com.chamil.ShopMate.model.orderEntity;
+import com.chamil.ShopMate.model.userEntity;
+import com.chamil.ShopMate.request.OrderRequest;
+import com.chamil.ShopMate.service.OrderService;
+import com.chamil.ShopMate.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/admin")
+public class AdminOrderController {
+    @Autowired
+    private OrderService orderService;
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/order/shop/{id}")
+    public ResponseEntity<List<orderEntity>> getOrderHistory(
+            @PathVariable Long id,
+            @RequestParam(required = false) String order_status,
+            @RequestHeader("Authorization") String jwt
+    ) throws Exception {
+        userEntity user = userService.findUserByJwtToken(jwt);
+        List<orderEntity> orders = orderService.getShopsOrder(id, order_status);
+
+        return new ResponseEntity<>(orders, HttpStatus.OK);
+    }
+
+    @PutMapping("/order/{id}/{order_status}")
+    public ResponseEntity<orderEntity> updateOrderStatus(
+            @PathVariable Long id,
+            @PathVariable String order_status,
+            @RequestHeader("Authorization") String jwt
+    ) throws Exception {
+        userEntity user = userService.findUserByJwtToken(jwt);
+        orderEntity orders = orderService.updateOrder(id, order_status);
+
+        return new ResponseEntity<>(orders, HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/chamil/ShopMate/controller/OrderController.java
@@ -0,0 +1,45 @@
+package com.chamil.ShopMate.controller;
+
+import com.chamil.ShopMate.model.categoryEntity;
+import com.chamil.ShopMate.model.orderEntity;
+import com.chamil.ShopMate.model.userEntity;
+import com.chamil.ShopMate.request.OrderRequest;
+import com.chamil.ShopMate.service.OrderService;
+import com.chamil.ShopMate.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class OrderController {
+    @Autowired
+    private OrderService orderService;
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/order")
+    public ResponseEntity<orderEntity> createOrder(
+            @RequestBody OrderRequest req,
+            @RequestHeader("Authorization") String jwt
+    ) throws Exception {
+        userEntity user = userService.findUserByJwtToken(jwt);
+        orderEntity createOrder = orderService.createOrder(req, user);
+
+        return new ResponseEntity<>(createOrder, HttpStatus.OK);
+    }
+
+    @GetMapping("/order/user")
+    public ResponseEntity<List<orderEntity>> getOrderHistory(
+            @RequestHeader("Authorization") String jwt
+    ) throws Exception {
+        userEntity user = userService.findUserByJwtToken(jwt);
+        List<orderEntity> orders = orderService.getUsersOrder(user.getId());
+
+        return new ResponseEntity<>(orders, HttpStatus.OK);
+    }
+}
